bee500c471499a43010e48c056486a757d6147ea
Syoffi Irzayuni <2411081040@student.pnp.ac.id>
2026-06-18 09:13:14 +0700

    fix: resolve seeder issues and improve branch management views

--- database/seeders/CabangSeeder.php
+++ database/seeders/CabangSeeder.php
@@ -18,24 +18,32 @@ class CabangSeeder extends Seeder
                 'alamat_cabang' => 'Jl. Purus IV No.5, Purus, Kota Padang',
                 'kontak_cabang' => '08123456789',
                 'jam_operasional' => '24 Hours',
+                'foto_cabang' => 'images/branch/nud.jpg',
+                'link_maps' => 'https://maps.app.goo.gl/rrPX2J77NUhnEpwi9',
             ],
             [
                 'nama_cabang' => 'Palióspíti Coffee & Roastery',
                 'alamat_cabang' => 'Jl. Kamang, Jati Baru, Kota Padang',
                 'kontak_cabang' => '08198765432',
                 'jam_operasional' => '10:00 - 04:00',
+                'foto_cabang' => 'images/branch/paliospiti.jpg',
+                'link_maps' => 'https://maps.app.goo.gl/eEoY2MsrfTPyKeYL7',
             ],
             [
                 'nama_cabang' => 'Pirzycoffee & Eatery',
                 'alamat_cabang' => 'Jl. Pulai, Simpang Haru, Kota Padang',
                 'kontak_cabang' => '08567891234',
                 'jam_operasional' => '24 Hours',
+                'foto_cabang' => 'images/branch/pirzy.jpg',
+                'link_maps' => 'https://maps.app.goo.gl/cngQEeep1DmWCSmF7',
             ],
             [
                 'nama_cabang' => 'Waroeng Raden',
                 'alamat_cabang' => 'Jl. Jaksa Agung R. Soeprapto No.7, Kota Padang',
                 'kontak_cabang' => '082112223333',
                 'jam_operasional' => '10:00 - 06:00',
+                'foto_cabang' => 'images/branch/raden.png',
+                'link_maps' => 'https://maps.app.goo.gl/q8emUyhMMwCXLVP78',
             ],
         ];
 
